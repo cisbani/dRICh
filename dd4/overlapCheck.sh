7c1b4c7343e3fefbfe9fbbe197d91d60cdd8613c
christopher dilks <christopher.j.dilks@gmail.com>
2021-07-19 18:01:17 -0400

    update dev scripts

--- dd4/overlapCheck.sh
+++ dd4/overlapCheck.sh
@@ -13,13 +13,19 @@ if [ $# -ne 1 ]; then
 fi
 
 xmlfile=${DRICH_DD4_ATHENA}/athena.xml
+wdir=$(pwd)
 
 case $1 in
   1)
     checkOverlaps -c $xmlfile
     ;;
   2)
-    python ${DRICH_DD4_ATHENA}/scripts/checkOverlaps.py -c $xmlfile
+    python ${DRICH_DD4_ATHENA}/scripts/checkOverlaps.py -c $xmlfile 2>&1 | tee tempo.log
+    grep -E 'Overlap.is.detected.for' tempo.log -A1 |\
+      sed 's/^.*for volume //g;s/^.* with //g' |\
+      sed 's/ volume.s//g' |\
+      tee $wdir/overlaps.log
+    rm tempo.log
     ;;
   *)
     echo "ERROR: unknown check"
