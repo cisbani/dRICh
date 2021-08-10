6ca1c01fee6e46c2c02a97589d5dd74f36ee1a3e
christopher dilks <christopher.j.dilks@gmail.com>
2021-08-10 15:51:01 -0400

    debugging pixel mapping

--- dd4/drawSegmentation.cpp
+++ dd4/drawSegmentation.cpp
@@ -1,52 +1,196 @@
 // test readout segmentation
 #include <cstdlib>
 #include <iostream>
+#include <bitset>
+#include <map>
 
 // ROOT
 #include "TSystem.h"
 #include "TCanvas.h"
 #include "TApplication.h"
 #include "ROOT/RDataFrame.hxx"
+#include "ROOT/RDFHelpers.hxx" // for RDF::RunGraphs
+
+using std::cout;
+using std::cerr;
+using std::endl;
 
 using namespace ROOT;
+using lvec = VecOps::RVec<Long64_t>;
+
 int main(int argc, char** argv) {
+
+  // setup
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // define application environment, to keep canvases open
   TApplication mainApp("mainApp",&argc,argv);
 
+  // enable multi-threading
+  EnableImplicitMT();
+
+  // read tree into dataframe
   RDataFrame dfIn("events","out/sim_run.root");
 
-  auto decodeID = [](VecOps::RVec<Long64_t> id, std::pair<int,int> b) {
-    int bitshift = b.first;
-    int length = b.second;
-    Long64_t div = (Long64_t) pow(2,bitshift);
+  // load sensor position LUT
+  std::map<Long64_t,std::pair<Long64_t,Long64_t>> modCoordMap;
+  TTree *modCoordTr = new TTree();
+  modCoordTr->ReadFile("text/sensorLUT.dat","module/L:x/F:y/F");
+  Long64_t moduleSens,xSens,ySens; Float_t xSensF,ySensF;
+  modCoordTr->SetBranchAddress("module",&moduleSens);
+  modCoordTr->SetBranchAddress("x",&xSensF);
+  modCoordTr->SetBranchAddress("y",&ySensF);
+  for(Long64_t e=0; e<modCoordTr->GetEntries(); e++) {
+    modCoordTr->GetEntry(e);
+    xSens = (Long64_t)(xSensF+0.5);
+    ySens = (Long64_t)(ySensF+0.5);
+    modCoordMap.insert(std::pair<Long64_t,std::pair<Long64_t,Long64_t>>(
+        moduleSens, std::pair<Long64_t,Long64_t>(xSens,ySens)
+	));
+  };
+  
+
+  // lambdas
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // decode cellID vector, given offset and length
+  auto decodeID = [] (lvec ids, int offset, int length) {
+    Long64_t div = (Long64_t) pow(2,offset);
     Long64_t mask = (Long64_t) pow(2,length) - 1;
-    return (id/div) & mask;
+    auto result = (ids/div) & mask;
+    return result;
+  };
+  // decoder for signed indicators (conversion may not be correct)
+  /*auto decodeIDsigned = [] (lvec ids, int offset, int length) {
+    Long64_t div = (Long64_t) pow(2,offset);
+    Long64_t mask = (Long64_t) pow(2,length) - 1;
+    lvec result;
+    for(auto i : ids) {
+      auto r = (i >> offset) & mask;
+      if(r>=pow(2,length-2)) r -= (Long64_t) pow(2,length-1);
+      result.emplace_back(r);
+    };
+    // debugging prints
+    for(auto b : ids) cout << std::bitset<64>(b) << endl;
+    cout << "(" << offset << "," << length << ") " << result << endl;
+    for(auto b : result) cout << std::bitset<16>(b) << endl;
+    return result;
+  };*/
+  // decoders: indicator -> cellID offset and length
+  auto detDecode = [&decodeID] (lvec ids) { return decodeID(ids,0,8); };
+  auto secDecode = [&decodeID] (lvec ids) { return decodeID(ids,8,3); };
+  auto modDecode = [&decodeID] (lvec ids) { return decodeID(ids,11,12); };
+  auto xDecode   = [&decodeID] (lvec ids) { return decodeID(ids,23,16); };
+  auto yDecode   = [&decodeID] (lvec ids) { return decodeID(ids,39,16); };
+
+
+  // convert module number to module histogram position
+  auto modCoordLU = [&modCoordMap] (lvec mods, int c) {
+    lvec result;
+    Double_t pos;
+    for(auto m : mods) {
+      try { pos = (c==0) ? modCoordMap[m].first : modCoordMap[m].second; } 
+      catch (const std::out_of_range &ex) {
+	cerr << "ERROR: cannot find module " << m << endl;
+	pos = 0;
+      };
+      result.emplace_back(pos);
+    };
+    return result;
+  };
+  auto modCoordX = [&modCoordLU] (lvec ids) { return modCoordLU(ids,0); };
+  auto modCoordY = [&modCoordLU] (lvec ids) { return modCoordLU(ids,1); };
+
+  // convert (module,segment) to pixel histogram position
+  Long64_t dilation = 5;
+  auto pixelCoord = [&dilation] (lvec modXs, lvec segXs) {
+    return dilation*modXs + segXs;
   };
 
-  auto dfBits = dfIn
-      .Define("detB", "std::pair<int,int> p{0,8}; return p")
-      .Define("secB", "std::pair<int,int> p{8,8}; return p")
-      .Define("modB", "std::pair<int,int> p{16,16}; return p")
-      .Define("xB",   "std::pair<int,int> p{32,16}; return p")
-      .Define("yB",   "std::pair<int,int> p{48,16}; return p")
+
+  // transformations
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // decode cellID
+  auto dfDecoded = dfIn
+      .Alias("id","DRICHHits.cellID")
+      .Define("det", detDecode, {"id"})
+      .Define("sec", secDecode, {"id"})
+      .Define("mod", modDecode, {"id"})
+      .Define("segX",   xDecode, {"id"})
+      .Define("segY",   yDecode, {"id"})
       ;
 
-  auto dfDecoded = dfBits
-      .Define("detDR", decodeID, {"DRICHHits.cellID","detB"})
-      .Define("secDR", decodeID, {"DRICHHits.cellID","secB"})
-      .Define("modDR", decodeID, {"DRICHHits.cellID","modB"})
-      .Define("xDR",   decodeID, {"DRICHHits.cellID","xB"})
-      .Define("yDR",   decodeID, {"DRICHHits.cellID","yB"})
+  // convert modules to histogram positions
+  auto dfModded = dfDecoded
+      .Define("modX", modCoordX, {"mod"})
+      .Define("modY", modCoordY, {"mod"})
       ;
 
+  // convert (module,segment) positions to pixel histogram position
+  auto dfPixels = dfModded
+      .Define("pixelX", pixelCoord, {"modX","segX"})
+      .Define("pixelY", pixelCoord, {"modY","segY"})
+      ;
+
+  // final dataframe
+  auto dfFinal = dfPixels;
+
+
+  // actions
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // book histograms
+  auto detHist = dfFinal.Histo1D("det");
+  auto secHist = dfFinal.Histo1D("sec");
+  auto modHist = dfFinal.Histo1D("mod");
+  auto xHist = dfFinal.Histo1D("segX");
+  auto yHist = dfFinal.Histo1D("segY");
+  auto sectorVsXY = dfFinal.Histo3D(
+      { "sectorVsXY","sector vs. xy;x;y;sec",
+	200,-2000,2000,200,-2000,2000,6,0,6 },
+      "DRICHHits.position.x","DRICHHits.position.y","sec"
+      );
+
+  // pixel hits
+  Double_t pixelXmin = dilation * 125;
+  Double_t pixelXmax = dilation * 200;
+  Double_t pixelYmin = dilation * -65;
+  Double_t pixelYmax = dilation * 65;
+  auto pixelHits = dfFinal.Histo3D(
+      { "pixelHits","pixel hits;x;y;sector",
+        (Int_t)(pixelXmax-pixelXmin), pixelXmin, pixelXmax,
+        (Int_t)(pixelYmax-pixelYmin), pixelYmin, pixelYmax,
+	6,0,6 },
+      "pixelX","pixelY","sec"
+      );
+
+  // execute concurrently
+  RDF::RunGraphs({ detHist, secHist, modHist, xHist, yHist, pixelHits });
+
+
+  // execution
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // draw
   TCanvas * c = new TCanvas();
   c->Divide(3,2);
-  c->cd(1); dfDecoded.Histo1D("detDR")->DrawClone();
-  c->cd(2); dfDecoded.Histo1D("secDR")->DrawClone();
-  c->cd(3); dfDecoded.Histo1D("modDR")->DrawClone();
-  c->cd(4); dfDecoded.Histo1D("xDR")->DrawClone();
-  c->cd(5); dfDecoded.Histo1D("yDR")->DrawClone();
+  for(int pad=1; pad<=6; pad++) c->GetPad(pad)->SetLogy();
+  c->cd(1); detHist->Draw();
+  c->cd(2); secHist->Draw();
+  c->cd(3); modHist->Draw();
+  c->cd(4); xHist->Draw();
+  c->cd(5); yHist->Draw();
 
-  std::cout << "\n\npress ^C to exit.\n\n";
+  //c = new TCanvas(); sectorVsXY->Draw("box");
+  c = new TCanvas();
+  //pixelHits->Draw("box");
+  Int_t secBin = pixelHits->GetZaxis()->FindBin(0.0); // limit to sector 0
+  pixelHits->GetZaxis()->SetRange(secBin,secBin);
+  TH2D *pixelHitsSec0 = (TH2D*) pixelHits->Project3D("yx");
+  pixelHitsSec0->Draw("colz");
+
+  cout << "\n\npress ^C to exit.\n\n";
   mainApp.Run();
   return 0;
 };
